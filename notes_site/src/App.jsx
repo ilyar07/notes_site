450e4c525ef132d3b5ac30fe9e9ed1e7c72d9d87
Илья <irotgan777@mail.ru>
2026-05-14 23:40:13 +0500

    добавлена сортировка по поисковой строке

--- notes_site/src/App.jsx
+++ notes_site/src/App.jsx
@@ -5,6 +5,8 @@ function App() {
     const [title, setTitle] = useState('');
     const [text, setText] = useState('');
     const [notes, setNotes] = useState([]);
+    const [search, setSearch] = useState('');
+
 
     const addNotes = () => {
         if (!title && !text) return;
@@ -20,11 +22,26 @@ function App() {
         setText('');
     }
 
+    const filterNotes = notes.filter(note => {
+        const lowerSearch = search.toLowerCase();
+        return note.title.toLowerCase().includes(lowerSearch) || note.text.toLowerCase().includes(lowerSearch);
+    })
+
+
     return (
         <div className="app">
             <h1 className="app__title">Мои заметки</h1>
 
             <div className="form">
+                <div className="search">
+                <input
+                    type="text"
+                    placeholder="Поиск по заметкам..."
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
+                    className="search__input"
+                    />
+                </div>
                 <input
                     type="text"
                     placeholder="Заголовок"
@@ -53,7 +70,7 @@ function App() {
             </div>
 
             <div className="notes-list">
-                {notes.map(note => (
+                {filterNotes.map(note => (
                     <div key={note.id} className="note-card">
                         <h3 className="note-card__title">{note.title || 'Без заголовка'}</h3>
                         <p className="note-card__text">{note.text}</p>
