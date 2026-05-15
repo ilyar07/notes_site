be1d80933be70c4f118f007ac1a09c496aa7da21
Илья <irotgan777@mail.ru>
2026-05-15 17:34:13 +0500

    исправлено лишнее сохранение в locaslStorage

--- notes_site/src/services/noteCRUD.js
+++ notes_site/src/services/noteCRUD.js
@@ -1,12 +1,6 @@
 const STORAGE_KEY = 'notes';
 
 
-// вспомогателная функция сохранения заметки в localStorage конкретно для этого файла
-const saveNote = (note) => {
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(note));
-}
-
-
 // (CREATE) создание заметки
 export const createNote = (title, text) => {
     const saved = getNotes();
@@ -17,23 +11,20 @@ export const createNote = (title, text) => {
         createdAt: new Date().toLocaleString()
     }
 
-    const updateNotes = [newNote, ...saved];
-    saveNote(updateNotes);
-    return updateNotes;
+    const updatedNotes = [newNote, ...saved];
+    return updatedNotes;
 }
 
 // (READ) получение всех заметок
 export const getNotes = () => {
     const saved = localStorage.getItem(STORAGE_KEY);
-
     return saved ? JSON.parse(saved) : [];
 }
 
 // (DELETE) удаление заметки
 export const deleteNote = (id) => {
     const saved = getNotes();
-    const updateNotes = saved.filter((note) => note.id !== id);
-    saveNote(updateNotes);
-    return updateNotes;
+    const updatedNotes = saved.filter((note) => note.id !== id);
+    return updatedNotes;
 }
 
